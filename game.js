1f78399d8a7ec12682f67e2fd094dabc65ae7b1c
karim-naviaux <karim.naviaux@student.vinci.be>
2023-11-03 22:28:40 +0100

    refactor: game.js

--- game.js
+++ game.js
@@ -208,40 +208,34 @@ function startTimer() {
 
 // Change le texte affiché en fonction du joueur
 function switchText() {
-  // textGameData.textTable = [
-  //   "C'est l'heure de la bataille!",
-  //   "Prêt pour le duel ultime?",
-  //   "Défends-toi avec détermination!",
-  //   "J'espère que le combat sera inoubliable!",
-  //   "La victoire est à portée de main!",
-  //   "Ne laisse pas ton adversaire t'échapper!",
-  //   "Les héros se lèvent dans l'arène!",
-  //   "Le combat commence, tiens bon!",
-  //   "L'épique combat des titans commence!",
-  //   "Que la force soit avec toi!",
-  //   "La fureur du combat t'envahit!",
-  //   "Le destin est entre tes mains!",
-  //   "La gloire t'attend au bout du chemin!",
-  //   "N'abandonne jamais, guerrier!",
-  //   "Affronte tes peurs, conquiers le combat!",
-  //   "L'arène résonne de cris de victoire!",
-  //   "Saisis ta destinée et combat!",
-  //   "Les étoiles te guideront vers la victoire!",
-  //   "L'honneur est ta récompense!",
-  //   "Ton courage est ton arme la plus puissante!",
-  //   "Rien n'est impossible pour un vrai guerrier!",
-  //   "Ta légende s'écrit dans le sang du combat!",
-  //   "L'adversité te rend plus fort!",
-  //   "Triomphe dans la chaleur du combat!",
-  //   "Le monde regarde, sois un champion!",
-  // ];
-
   textGameData.textTable = [
-    "je m'appelle karim", 
-    "je m'appelle kamel", 
-    "je m'appelle sofia", 
-    "je m'appelle kaka"
-  ]
+    "C'est l'heure de la bataille!",
+    "Prêt pour le duel ultime?",
+    "Défends-toi avec détermination!",
+    "J'espère que le combat sera inoubliable!",
+    "La victoire est à portée de main!",
+    "Ne laisse pas ton adversaire t'échapper!",
+    "Les héros se lèvent dans l'arène!",
+    "Le combat commence, tiens bon!",
+    "L'épique combat des titans commence!",
+    "Que la force soit avec toi!",
+    "La fureur du combat t'envahit!",
+    "Le destin est entre tes mains!",
+    "La gloire t'attend au bout du chemin!",
+    "N'abandonne jamais, guerrier!",
+    "Affronte tes peurs, conquiers le combat!",
+    "L'arène résonne de cris de victoire!",
+    "Saisis ta destinée et combat!",
+    "Les étoiles te guideront vers la victoire!",
+    "L'honneur est ta récompense!",
+    "Ton courage est ton arme la plus puissante!",
+    "Rien n'est impossible pour un vrai guerrier!",
+    "Ta légende s'écrit dans le sang du combat!",
+    "L'adversité te rend plus fort!",
+    "Triomphe dans la chaleur du combat!",
+    "Le monde regarde, sois un champion!",
+  ];
+
   const randomIndex = Math.floor(Math.random() * textGameData.textTable.length);
   textGameData.phrase = textGameData.textTable[randomIndex];
   console.log(textGameData.phrase);
